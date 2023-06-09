01dec79c07bfa18cfea8b3a0ef09b8f11e026247
demoB33 <vasilenko-s@list.ru>
2023-06-09 13:24:06 +0700

    Initial commit

---
+++ src/test/java/com/example/coursework2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
